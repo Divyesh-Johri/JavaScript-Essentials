23a9ca908569f51fcc21f791f19e152c6adfce81
Divyesh Johri <divyeshjohri@gmail.com>
2020-05-15 16:15:29 -0400

    Chapter 2 complete

---
+++ Exercise Files/02_04/script.js
@@ -0,0 +1,6 @@
+var date = new Date();
+document.body.innerHTML = "<h1>Today is: " + date + "</h1>";
+
+// Javascript conventions
+//  - you actually don't need semicolons, but its recommended as convention
+//  - same common naming conventions
