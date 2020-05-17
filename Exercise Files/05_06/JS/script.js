b1316ed485d7dfa064b282fb68a8b0c5dc59eb58
Divyesh Johri <divyeshjohri@gmail.com>
2020-05-17 16:47:05 -0400

    Chapter 5 complete

--- Exercise Files/05_06/JS/script.js
+++ Exercise Files/05_06/JS/script.js
@@ -1,1 +1,24 @@
+// Inserting elements into DOM (in a way that supports all browsers)
 
+// Find the targeted image
+const FEATURED = document.querySelector(".featured-image")
+const THEIMAGE = FEATURED.querySelector("img")
+
+var altText = THEIMAGE.getAttribute("alt")
+
+// create figcaption element
+var captionElement = document.createElement("figcaption")
+
+// create and append image text to new element node
+// 
+// captionElement.append(altText)
+// ^^ This could work too, but isn't supported by all browsers
+var captionText = document.createTextNode(altText)
+captionElement.appendChild(captionText)
+
+
+// append the element to the FEATURED node in the DOM
+FEATURED.appendChild(captionElement)
+
+// fix the original image element
+THEIMAGE.setAttribute("alt","")
